737d7a03febe98ed9917783ea67d7105a30ae4c6
root <root@ost.dev.eggpire.com>
2012-12-06 04:03:50 -0500

    added new brands manager added currency support added in clause suport to prepwhere cleaned up tax logic cleaned up database for new subtotal item logic added new item presonalities

--- lib/contact.php
+++ lib/contact.php
@@ -2,6 +2,10 @@
 
 class Contact {
 
+	const SHIP = 'ship';
+	const BILL = 'bill';
+	const BOTH = 'both';
+
 	protected static function _fetch(&$pairs,$all=false){
 		$select = 'SELECT * FROM `contacts`';
 		$where = Db::prepwhere($pairs);
@@ -20,6 +24,15 @@ class Contact {
 	public static function allByAccount($account_id){
 		return self::fetchAll(array(
 			 'contact_account_id'	=> $account_id
+			,'contact_is_active'	=> 1
+			)
+		);
+	}
+
+	public static function allByAccountAndType($account_id,$contact_type){
+		return self::fetchAll(array(
+			 'contact_account_id'	=> $account_id
+			,'contact_type'			=> $contact_type
 			,'contact_is_active'	=> 1
 			)
 		);
@@ -95,5 +108,33 @@ class Contact {
 	public static function deactivate($contact_id){
 		return Db::_get()->update('contacts','contact_id',$contact_id,array('contact_is_active'=>0));
 	}
+
+	public static function contactDrop($account_id=null,$contact_type=null,$value=null,$name='contact_id'){
+		lib('form_drop');
+		$arr = array();
+		foreach(self::allByAccountAndType($account_id,$contact_type) as $contact)
+			$arr[$contact['contact_id']] =
+				 ((strlen($contact['first_name'])>0)?$contact['first_name'].' ':'')
+				.((strlen($contact['last_name'])>0)?$contact['last_name']:'')
+				.((strlen($contact['email'])>0)?' <'.$contact['email'].'>':'')
+			;
+		$drop = FormDrop::_get()->setOptions($arr);
+		$drop->setName($name);
+		$drop->setValue($value);
+		return $drop;
+	}
+
+	public static function formatAddressLong(&$data){
+		$address = (mda_get($data,'address_name'))?mda_get($data,'address_name').PHP_EOL:'';
+		$address .= (mda_get($data,'address_1'))?mda_get($data,'address_1').PHP_EOL:'';
+		$address .= (mda_get($data,'address_2'))?mda_get($data,'address_2').PHP_EOL:'';
+		$address .= (mda_get($data,'city'))?mda_get($data,'city').', ':'';
+		$address .= (mda_get($data,'state'))?mda_get($data,'state').'  ':'';
+		$address .= (mda_get($data,'zip'))?mda_get($data,'zip').' ':'';
+		$address .= (mda_get($data,'country'))?mda_get($data,'country'):'';
+		$address .= (strlen($address)>0)?PHP_EOL:'';
+		return $address;
+	}
+
 	
 }
