10a45a456c2fa3ad40003fa4d11f90b31125cab7
root <root@ost.dev.eggpire.com>
2012-12-07 22:52:58 -0500

    new account management structure with db structure added brand management added tax class management added tax rule management added currency management move src. to func.d which is the proper name restructured form helpers added FormCheckbox helper

--- lib/contact.php
+++ lib/contact.php
@@ -1,87 +1,94 @@
 <?php
 
-class Contact {
+abstract class Contact {
 
 	const SHIP = 'ship';
 	const BILL = 'bill';
 	const BOTH = 'both';
 
-	protected static function _fetch(&$pairs,$all=false){
-		$select = 'SELECT * FROM `contacts`';
+	static $accounts_table = 'clients';
+	static $contacts_table = 'contacts';
+
+	final protected static function _fetch(&$pairs,$all=false){
+		$select = 'SELECT * FROM `'.static::$contacts_table.'`';
 		$where = Db::prepwhere($pairs);
 		$func = ($all?'fetchAll':'fetch');
 		return Db::_get()->$func($select.array_shift($where),$where);
 	}
 	
-	protected static function fetch($pairs=array()){
+	final protected static function fetch($pairs=array()){
 		return self::_fetch(&$pairs,false);
 	}
 
-	protected static function fetchAll($pairs=array()){
+	final protected static function fetchAll($pairs=array()){
 		return self::_fetch(&$pairs,true);
 	}
 
-	public static function allByAccount($account_id){
+	final public static function allByAccount($account_id){
 		return self::fetchAll(array(
-			 'contact_account_id'	=> $account_id
-			,'contact_is_active'	=> 1
+			 'contact_account_id'		=> $account_id
+			,'contact_account_table'	=> static::$accounts_table
+			,'contact_is_active'		=> 1
 			)
 		);
 	}
 
-	public static function allByAccountAndType($account_id,$contact_type){
+	final public static function allByAccountAndType($account_id,$contact_type){
+		if(!is_array($contact_type)) $contact_type = array($contact_type);
 		return self::fetchAll(array(
-			 'contact_account_id'	=> $account_id
-			,'contact_type'			=> $contact_type
-			,'contact_is_active'	=> 1
+			 'contact_account_id'		=> $account_id
+			,'contact_account_table'	=> static::$accounts_table
+			,'contact_type'				=> array('AND',Db::IN,$contact_type)
+			,'contact_is_active'		=> 1
 			)
 		);
 	}
 	
-	public static function get($contact_id){
+	final public static function get($contact_id){
 		return self::fetch(array('contact_id'=>$contact_id));
 	}
 	
-	public static function createParams(){
+	final public static function createParams(){
 		return array(
-			 'first_name'		=> ''
-			,'last_name'		=> ''
-			,'email'			=> ''
-			,'phone'			=> ''
-			,'address_name'		=> ''
-			,'address_1'		=> ''
-			,'address_2'		=> ''
-			,'city'				=> ''
-			,'state'			=> ''
-			,'zip'				=> ''
-			,'country'			=> 'US'
+			 'first_name'	=> ''
+			,'last_name'	=> ''
+			,'email'		=> ''
+			,'phone'		=> ''
+			,'address_name'	=> ''
+			,'address_1'	=> ''
+			,'address_2'	=> ''
+			,'city'			=> ''
+			,'state'		=> ''
+			,'zip'			=> ''
+			,'country'		=> 'US'
 		);
 	}
 	
-	public static function create($account_id,$data){
+	final public static function create($account_id,$data){
 		return Db::_get()->insert(
-			 'contacts'
+			 static::$contacts_table
 			,array(
-				 'contact_account_id'	=> $account_id
-				,'first_name'			=> mda_get($data,'first_name')
-				,'last_name'			=> mda_get($data,'last_name')
-				,'email'				=> mda_get($data,'email')
-				,'contact_password'		=> bcrypt(mda_get($data,'password'))
-				,'contact_is_active'	=> 1
-				,'contact_created'		=> time()
-				,'phone'				=> mda_get($data,'phone')
-				,'address_name'			=> mda_get($data,'address_name')
-				,'address_1'			=> mda_get($data,'address_1')
-				,'address_2'			=> mda_get($data,'address_2')
-				,'city'					=> mda_get($data,'city')
-				,'state'				=> mda_get($data,'state')
-				,'zip'					=> mda_get($data,'zip')
-				,'country'				=> mda_get($data,'country')
+				 'contact_account_id'		=> $account_id
+				,'contact_account_table'	=> static::$accounts_table
+				,'first_name'				=> mda_get($data,'first_name')
+				,'last_name'				=> mda_get($data,'last_name')
+				,'email'					=> mda_get($data,'email')
+				,'contact_password'			=> bcrypt(mda_get($data,'password'))
+				,'contact_is_active'		=> 1
+				,'contact_created'			=> time()
+				,'phone'					=> mda_get($data,'phone')
+				,'address_name'				=> mda_get($data,'address_name')
+				,'address_1'				=> mda_get($data,'address_1')
+				,'address_2'				=> mda_get($data,'address_2')
+				,'city'						=> mda_get($data,'city')
+				,'state'					=> mda_get($data,'state')
+				,'zip'						=> mda_get($data,'zip')
+				,'country'					=> mda_get($data,'country')
 			)
 		);
 	}
 	
-	public static function update($contact_id,$data){
+	final public static function update($contact_id,$data){
 		$params = array(
 			 'first_name'		=> mda_get($data,'first_name')
 			,'last_name'		=> mda_get($data,'last_name')
@@ -98,21 +105,21 @@ class Contact {
 		//add the password if it changed
 		if(mda_get($data,'password')) $params['password'] = bcrypt(mda_get($data,'password'));
 		return Db::_get()->update(
-			 'contacts'
+			 static::$accounts_table
 			,'contact_id'
 			,$contact_id
 			,$params
 		);
 	}
 	
-	public static function deactivate($contact_id){
-		return Db::_get()->update('contacts','contact_id',$contact_id,array('contact_is_active'=>0));
+	final public static function deactivate($contact_id){
+		return Db::_get()->update(static::$accounts_table,'contact_id',$contact_id,array('contact_is_active'=>0));
 	}
 
-	public static function contactDrop($account_id=null,$contact_type=null,$value=null,$name='contact_id'){
+	final public static function contactDrop($account_id=null,$contact_type=null,$value=null,$name='contact_id'){
 		lib('form_drop');
 		$arr = array();
-		foreach(self::allByAccountAndType($account_id,$contact_type) as $contact)
+		foreach(self::allByAccountAndType($account_id,array($contact_type,self::BOTH)) as $contact)
 			$arr[$contact['contact_id']] =
 				 ((strlen($contact['first_name'])>0)?$contact['first_name'].' ':'')
 				.((strlen($contact['last_name'])>0)?$contact['last_name']:'')
@@ -124,7 +131,13 @@ class Contact {
 		return $drop;
 	}
 
-	public static function formatAddressLong(&$data){
+	final public static function formatPhone(&$data){
+		$p = mda_get($data,'phone');
+		if(($p === '')||($p===null)) return '';
+		return sprintf('(%03d)%03d-%04d',substr($p,0,3),substr($p,3,3),substr($p,6));
+	}
+
+	final public static function formatAddressLong(&$data){
 		$address = (mda_get($data,'address_name'))?mda_get($data,'address_name').PHP_EOL:'';
 		$address .= (mda_get($data,'address_1'))?mda_get($data,'address_1').PHP_EOL:'';
 		$address .= (mda_get($data,'address_2'))?mda_get($data,'address_2').PHP_EOL:'';
@@ -136,5 +149,4 @@ class Contact {
 		return $address;
 	}
 
-	
 }
