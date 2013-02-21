135651362ff48b9a4b360a08767c8b912e4b5294
Nullivex <contact@nullivex.com>
2013-02-20 21:46:38 -0800

    name abstractions in the URL lib in response to #72

--- example/url/50_client_session.php
+++ example/url/50_client_session.php
@@ -1,8 +1,8 @@
 <?php
 
-Url::register('staff',Url::home().'?act=staff');
-Url::register('staff_create',Url::staff().'&do=create');
-Url::register('staff_manage',Url::staff().'&do=manage&staff_id=$staff_id',array('staff_id'));
-Url::register('profile',Url::staff().'&do=profile');
-Url::register('login',Url::staff().'&do=login');
-Url::register('logout',Url::staff().'&do=logout');
+Url::_register('staff',Url::home().'?act=staff');
+Url::_register('staff_create',Url::staff().'&do=create');
+Url::_register('staff_manage',Url::staff().'&do=manage&staff_id=$staff_id',array('staff_id'));
+Url::_register('profile',Url::staff().'&do=profile');
+Url::_register('login',Url::staff().'&do=login');
+Url::_register('logout',Url::staff().'&do=logout');
