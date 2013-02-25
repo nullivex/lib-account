30e1033b79e2432cd1aabcb98973f5b991eb5356
Nullivex <contact@nullivex.com>
2013-02-24 19:56:23 -0800

    phptal testing

--- example/ctl/staff_login.php
+++ example/ctl/staff_login.php
@@ -26,6 +26,8 @@ if(post('login')){
 }
 
 session('login_referrer',server('HTTP_REFERER'));
+
+/* OLD templating stuff
 page_header_admin(false);
 $params = array('url_login'=>htmlentities(Url::login()));
 Tpl::_get()->parse('login','page',$params);
@@ -34,3 +36,26 @@ Tpl::_get()->parse('login','page_end');
 Tpl::_get()->addCss(Config::get('tpl','theme_path').'/css/maruti-login.css');
 Tpl::_get()->addJs('/js/maruti.login.js');
 output(Tpl::_get()->output());
+ */
+
+//load new templating engine
+require_once(ROOT.'/usr/phptal/PHPTAL.php');
+
+$script_stats = 'Execution: 0.00613 | Queries: 0 | Memory: 1.58MB';
+
+$tpl = new PHPTAL(ROOT_GROUP.'/theme/default/login.xhtml');
+$tpl->theme_uri = '/theme/default';
+$tpl->site_title = Config::get('info','site_name').' Admin Login';
+$tpl->site_name = Config::get('info','site_name');
+$tpl->url_login = Url::login();
+$tpl->copyright = '© '.date('Y').' '.Config::get('info','site_name');
+$tpl->script_info = sprintf('Vidcache: v%s | OpenLSS: v%s | %s',VERSION,LSS_VERSION,$script_stats);
+$out = $tpl->execute();
+
+//cleanup the output
+$tidy = new tidy();
+$tidy->parseString($out,array('indent'=>true,'output-xml'=>true,'numeric-entities'=>true,'preserve-entities'=>true,'wrap'=>300),'utf8');
+$tidy->cleanRepair();
+
+//output
+echo $tidy;
